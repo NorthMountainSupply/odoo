5ed288132ea21fc92e26cd48e303c28fc0d57604
clh5602 <clh5602@rit.edu>
2025-01-22 14:59:21 -0500

    define computed field on template and product, figuring out how to add to view

---
+++ custom-addons/can-be-manufactured-field/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Can Be Manufactured Field",
+    "version": "1.0",
+    "category": "Product",
+    "summary": "Add a custom field to the product.product model",
+    "depends": ["product"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
