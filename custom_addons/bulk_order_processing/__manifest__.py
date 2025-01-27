03ec759b728ead6c45068e8108e36a2699c3608d
clh5602 <clh5602@rit.edu>
2025-01-27 11:55:31 -0500

    outline file structure, create __manifest__.py

---
+++ custom_addons/bulk_order_processing/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Bulk Order Processing",
+    "version": "1.0",
+    "category": "Sales",
+    "summary": "Adds a custom field to the product.product and product.template models.",
+    "author": "Colby Heaton",
+    "depends": ["product", "mrp", "sale_management"],
+    "data": [
+        "views/product_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
